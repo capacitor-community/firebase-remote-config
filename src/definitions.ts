ba38e8f806bfd58d20d3281a7c7fa690456c0c11
Priyank Patel <priyank.patel@stackspace.ca>
2020-05-29 19:37:08 -0400

    Update interfaces

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,5 +5,16 @@ declare module "@capacitor/core" {
 }
 
 export interface FirebaseRemoteConfigPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
+  initialize(options: { minimumFetchIntervalInSeconds: number }): Promise<void>;
+  fetch(): Promise<void>;
+  activate(): Promise<void>;
+  fetchAndActivate(): Promise<void>;
+  getBoolean(options: RCValueOption): Promise<{ key: string; value: boolean; source: string; }>;
+  getByteArray(options: RCValueOption): Promise<{ key: string; value: any[]; source: string; }>;
+  getNumber(options: RCValueOption): Promise<{ key: string; value: number; source: string; }>;
+  getString(options: RCValueOption): Promise<{ key: string; value: string; source: string; }>;
+}
+
+export interface RCValueOption {
+  key: string;
 }
--- src/web.ts
+++ src/web.ts
@@ -9,10 +9,40 @@ export class FirebaseRemoteConfigWeb extends WebPlugin implements FirebaseRemote
     });
   }
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
+  initialize(options: { minimumFetchIntervalInSeconds: number; }): Promise<void> {
+    console.log(options);
+    throw new Error("Method not implemented.");
   }
+
+  fetch(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  activate(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  fetchAndActivate(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  getBoolean(options: import("./definitions").RCValueOption): Promise<{ key: string; value: boolean; source: string; }> {
+    console.log(options);
+    throw new Error("Method not implemented.");
+  }
+
+  getByteArray(options: import("./definitions").RCValueOption): Promise<{ key: string; value: any[]; source: string; }> {
+    console.log(options);
+    throw new Error("Method not implemented.");
+  }
+  getNumber(options: import("./definitions").RCValueOption): Promise<{ key: string; value: number; source: string; }> {
+    console.log(options);
+    throw new Error("Method not implemented.");
+  }
+
+  getString(options: import("./definitions").RCValueOption): Promise<{ key: string; value: string; source: string; }> {
+    console.log(options);
+    throw new Error("Method not implemented.");
+  }
+  
 }
 
 const FirebaseRemoteConfig = new FirebaseRemoteConfigWeb();
